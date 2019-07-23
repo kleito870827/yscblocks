b5b27f50338f0c39f18ecbe03aa0a557a0d2614c
Caleo <caleo@Caleos-MacBook-Pro.local>
2019-07-23 10:58:49 -0400

    first commit

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:  YSC Blocks
+ * Description:  YSC, Blocks collection and extensions for Gutenberg
+ * Version:      @@plugin_version
+ * Author:       YSC, SpiderBoost
+ * Author URI:   https://yasielscaleo.com/
+ * License:      GPLv2 or later
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:  ysc
+ *
+ * @package ysc
+ */
